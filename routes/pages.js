b40c43a72eca22baa2cb1ba9507367844616612c
Poorvaj-HV <poorvajvishwanath19@gmail.com>
2025-11-14 20:57:53 +0530

    Switch email service from Gmail to Brevo SMTP

--- routes/pages.js
+++ routes/pages.js
@@ -40,11 +40,21 @@ function uploadBufferToCloudinary(buffer, folder = '', filename = '') {
 const otpStore = new Map();
 
 // Email transporter using Gmail App Password
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// });
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS
     }
 });
 
